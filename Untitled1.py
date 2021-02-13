054e26852a0cd5fb47f422255e7753b9e492c8a6
harshitakedia <78649300+harshitakedia@users.noreply.github.com>
2021-02-13 13:17:58 +0530

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#list of numbers
+list = [10, -2, 5, 6, -8, 12, -7, 30]
+#for loop
+for n in list:
+    #checking condition
+    if n > 0:
+        print(n, end = " ")
+
+
+# In[ ]:
+
+
+
+
